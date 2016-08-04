20947948af1690be73d5f2428ceca0f298c310ee
lenliscio <len.liscio@gmail.com>
2016-08-04 13:45:26 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include users
+  include users::admins
   include skeleton
   include memcached
   include nginx
