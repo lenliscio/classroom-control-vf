09433ed28fd061efb66e5d0d1353364c677071fa
lenliscio <len.liscio@gmail.com>
2016-08-04 09:56:18 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,13 +1,18 @@
 class nginx {
-  package { 'nginx':
-  ensure => present,
-}
-file { '/var/www':
-  ensure => directory,
+File {
   owner => 'root',
   group => 'root',
-  mode => '0775',
+  mode => '0664',
 }
+
+package { 'nginx':
+  ensure => present,
+}
+
+file { '/var/www':
+  ensure => directory,
+}
+
 file { '/var/www/index.html':
   ensure => file,
   owner => 'root',
@@ -17,28 +22,21 @@ file { '/var/www/index.html':
 }
 file { '/etc/nginx/nginx.conf':
   ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0664',
   source => 'puppet:///modules/nginx/nginx.conf',
   require => Package['nginx'],
   notify => Service['nginx'],
 }
 file { '/etc/nginx/conf.d':
   ensure => directory,
-  owner => 'root',
-  group => 'root',
-  mode => '0775',
  }
+ 
 file { '/etc/nginx/conf.d/default.conf':
   ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0664',
   source => 'puppet:///modules/nginx/default.conf',
   require => Package['nginx'],
   notify => Service['nginx'],
  }
+ 
 service { 'nginx':
   ensure => running,
   enable => true,
