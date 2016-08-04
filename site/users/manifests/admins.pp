fdfa605caa2917d9d5a7f979146e64157dd7ea6a
lenliscio <len.liscio@gmail.com>
2016-08-04 13:41:53 -0400

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -3,7 +3,7 @@ class users::admins {
   users::managed_user { 'alice':
   group => 'staff',
 }
-users::managed_user { 'aaron':
+users::managed_user { 'chen':
   group => 'staff',
 }
 group { 'staff':
